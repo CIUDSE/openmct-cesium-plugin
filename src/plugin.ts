e57977fc69209c25fc0f2b3b38bc18fd1fb75ded
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2023-11-24 00:38:46 +0000

    Added openmct type information

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,7 +5,6 @@ import type { DomainObject } from "../openmct/dist/src/api/objects/ObjectAPI";
 
 export default function CesiumPlugin() {
   return function install(openmct: OpenMCT) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     openmct.types.addType("cesium", {
       name: "Cesium",
       description: "Graphically visualize geodetic data",
